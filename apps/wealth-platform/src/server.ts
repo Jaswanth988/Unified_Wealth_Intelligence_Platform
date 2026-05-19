c99c36551217e5962811c38082faa9943fc37aba
JAswanthkumar <Sjaswanth7777@gmail.com>
2026-05-19 19:30:08 +0530

    work before pull

--- apps/wealth-platform/src/server.ts
+++ apps/wealth-platform/src/server.ts
@@ -1,14 +1,38 @@
 import express from 'express';
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+import realEstateRoutes from './routes/realEstate.routes';
+import authRoutes from './routes/auth.routes';
+import wealthRoutes from './routes/wealth.routes';
+import healthRoutes
+from "./routes/health.routes";
+import {
+  apiRateLimiter
+}
+from "./middleware/rateLimit.middleware";
+dotenv.config();
 
 const app = express();
 
-app.get('/', (_, res) => {
+app.use(express.json());
+app.use(apiRateLimiter);
+app.get('/', (_req, res) => {
   res.send('Wealth Platform Running');
 });
 
-app.get('/dashboard', async (_, res) => {
+app.use('/api/properties', realEstateRoutes);
+
+app.use('/api/auth', authRoutes);
+
+app.use("/api/wealth", wealthRoutes);
+app.use(
+  "/api/health",
+  healthRoutes
+);
+app.get('/dashboard', async (_req, res) => {
   try {
+
     const equity = await axios.get(
       'http://localhost:4001/health'
     );
@@ -22,7 +46,11 @@ app.get('/dashboard', async (_, res) => {
       equityService: equity.data,
       mfService: mf.data
     });
+
   } catch (error) {
+
+    console.log(error);
+
     res.status(500).json({
       message: 'One service failed'
     });
@@ -32,5 +60,5 @@ app.get('/dashboard', async (_, res) => {
 const PORT = 4000;
 
 app.listen(PORT, () => {
-  console.log(`Wealth Platform running on ${PORT}`);
+  console.log(`🚀 Wealth Platform running on ${PORT}`);
 });
